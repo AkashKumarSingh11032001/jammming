e95a4c77231fd58c580e3694ef454f829652cf1d
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-05-12 17:40:10 +0530

    constructor in App componets

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,6 +6,11 @@ import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        
+    }
+    
     render() {
         return (
         <div>
