58fb55777e47453a8e547c257a0910f59c56cce8
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-05-14 20:15:17 +0530

    api

--- src/components/util/Spotify.js
+++ src/components/util/Spotify.js
@@ -1,11 +1,27 @@
 import React from 'react';
 
-const accessToken = "";
+const clientId = "5da05ad2a9e946bfa3fe7e62dd54f97c";
+const redirectUri = "http://localhost:3000";
+let accessToken;
 
 const Spotify = {
     getAccessToken() {
         if(accessToken){
             return accessToken;
+        }
+        //  check for access tiken match
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+        
+        if(accessTokenMatch && expiresInMatch){
+            accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+            // grab new access token when it get expires
+            window.setTimeout(() => accessToken = "",expiresIn*1000);
+            window.history.pushState('Access Token', null, '/');
+        }
+        else {
+            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`
         }
     }
 
