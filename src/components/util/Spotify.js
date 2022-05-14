fd47714246bb54dc1b29937e7f55c536d4a3d430
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-05-14 20:20:34 +0530

    api

--- src/components/util/Spotify.js
+++ src/components/util/Spotify.js
@@ -26,7 +26,11 @@ const Spotify = {
     },
     search(term) {
         const accessToken = Spotify.getAccessToken(term);
-        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`)
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`,{
+            header: {
+                Authorization: `Bearer ${accessToken}`;
+            }
+        })
     }
 
 
